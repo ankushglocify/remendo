29208f9111078aafd3ac01c82fa18bae443651a8
dineshupwork1 <dineshupwork1@gmail.com>
2021-02-11 16:46:40 +0530

    Update profile.blade.php

--- resources/views/front/profile.blade.php
+++ resources/views/front/profile.blade.php
@@ -22,7 +22,7 @@
     <strong>{{ $message }}</strong>
     </div>
   @endif
-  <div class="container-fluid">
+  <div class="container">
     <div class="row">
       <div class="col-lg-12 col-md-12 col-sm-12">
         <div class="profileForm">
