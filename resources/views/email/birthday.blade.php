aaa922da4a4905f9b1ca25b707fb89f77b902ee2
dineshupwork1 <dineshupwork1@gmail.com>
2021-02-15 17:19:56 +0530

    Update birthday.blade.php

--- resources/views/email/birthday.blade.php
+++ resources/views/email/birthday.blade.php
@@ -86,7 +86,7 @@
     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;margin: 20px auto;box-shadow: 0 8px 30px rgb(0 0 0 / 20%);">
         <tr>
             <td class="emailContent" bgcolor="" align="center" style="">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;box-shadow: 0 8px 30px rgb(0 0 0 / 15%);margin: 20px auto;">
                 	<tr>
                         <td  align="center" valign="top" style="padding: 40px 0 0;background: url({{ asset('assets/images/service_shape-top.png') }});background-repeat: no-repeat;    background-size: 38% 100%;position: relative;background-color: #fff;">
                             <a href="{{url('/')}}" style=""><img style="width: 100%;max-width: 40%;" src="{{ asset('assets/images/logo.png') }}" alt=""/></a>
