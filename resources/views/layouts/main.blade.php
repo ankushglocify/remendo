8a40403d2b0f841b7d907f73c619d5e67f0095c4
dineshupwork1 <dineshupwork1@gmail.com>
2021-02-17 18:10:32 +0530

    title update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <title>{{ config('app.name', 'Laravel') }}</title>
+   <title>Remindio - A Simple Birthday Reminder App</title>
  
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
